fe17a6e6ef6942a93b2683c82a78bf8cb005ae00
algon31 <ravibhuvan31@gmail.com>
2025-08-13 21:28:59 +0530

    resolved url

--- backend/utilites/config.js
+++ backend/utilites/config.js
@@ -1,8 +1,8 @@
 
 const Check = "production"
 
-const PBackEndUrl = "http://localhost:3000";
-const LBackEndUrl = "https://chesso-ejb0.onrender.com";
+const LBackEndUrl = "http://localhost:3000";
+const PBackEndUrl = "https://chesso-ejb0.onrender.com";
 
 const BackEndUrl = Check === "production" ? PBackEndUrl : LBackEndUrl
 
